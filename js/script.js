f9f7e24fab85bcc6502430f47f8596578af629d1
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-01-10 17:14:44 +0100

    ma 1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Question 1
+
+var number = 2;
+var myBoolean = true;
+var string = "Hello i'm a string!"
+
+//------------------------------------------------------------
+// Question 2
+
+var firstName = "Espen";
+var lastName = "Pedersen";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//------------------------------------------------------------
+// Question 3
+
+var frog = "frog";
+var typeOf = typeof frog
+
+console.log("The type of frog is " + typeOf);
+
+//------------------------------------------------------------
+// Question 4
+
+var orderHasShipped = false;
+
+if (orderHasShipped === true) {
+    console.log("The order shipped");
+} else {
+    console.log("The order did not ship");
+}
+
+//------------------------------------------------------------
+// Question 5
+
+for (var i = 7; i < 14; i++) {
+    console.log(i);
+}
